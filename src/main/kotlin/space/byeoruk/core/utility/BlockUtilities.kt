f714c431091bf91f18b6492529775b93730d8524
qufnr <qufnr@icloud.com>
2026-03-04 01:58:08 +0900

    refactor(BlockUtilities): `isMaxAge` 메소드 추가

--- src/main/kotlin/space/byeoruk/core/utility/BlockUtilities.kt
+++ src/main/kotlin/space/byeoruk/core/utility/BlockUtilities.kt
@@ -3,6 +3,7 @@ package space.byeoruk.core.utility
 import org.bukkit.Material
 import org.bukkit.Tag
 import org.bukkit.block.Block
+import org.bukkit.block.data.Ageable
 
 object BlockUtilities {
     /**
@@ -30,4 +31,23 @@ object BlockUtilities {
      * @return 블록이 원목이거나 광석이면 true 아니면 false
      */
     fun isOreOrLog(block: Block): Boolean = isLog(block) || isOre(block)
+
+    /**
+     * 블록이 최대 나이인지 유무
+     *
+     * @param block 블록 데이터
+     * @return 블록이 최대 나이이면 true 아니면 false
+     */
+    fun isMaxAge(block: Block): Boolean {
+        //  열매랑 붙어있는 꼬다리일 경우에만 Max Age로 판단
+        if(block.type == Material.PUMPKIN_STEM || block.type == Material.MELON_STEM)
+            return false
+
+        if(block.blockData is Ageable) {
+            val ageable = block.blockData as Ageable
+            return ageable.age >= ageable.maximumAge
+        }
+
+        return false
+    }
 }
